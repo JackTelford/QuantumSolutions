68a0d01c2d2a7d400f013c227094e998a2bae590
Jack Telford <wipke9893@yahoo.com>
2025-02-14 10:53:25 -0500

    first push

---
+++ src/Themes/HallsCreekTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "HallsCreekTheme",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "HallsCreekTheme"
+)]
